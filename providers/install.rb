927f11d0dfa7c6e9ab6084c1b398787839b2bee8
Álvaro Faúndez <alvaro@faundez.net>
2015-08-06 06:16:11 -0300

    FIX centos compatibility with olver ruby versions

--- providers/install.rb
+++ providers/install.rb
@@ -2,11 +2,20 @@ action :install do
 
   user_root = (new_resource.user == 'root') ? '/root' : "/home/#{new_resource.user}"
 
+  patch = case node[:platform]
+  when 'ubuntu'
+    if Gem::Version.new(new_resource.name) <= Gem::Version.new('2.0.0-p247')
+      'curl -fsSL "https://github.com/ruby/ruby/commit/0d58bb55985e787364b0235e5e69278d0f0ad4b0.patch" | filterdiff -x a/ChangeLog | '
+    end
+  end
+
+  log "#{patch unless patch.nil?} rbenv install #{'--patch' unless patch.nil?} #{new_resource.name} && rbenv rehash"
+
   bash "rbenv install #{new_resource.name}" do
     user new_resource.user
     group new_resource.user
     flags '-l'
-    code "rbenv install #{new_resource.name} && rbenv rehash"
+    code "#{patch unless patch.nil?} rbenv install #{'--patch' unless patch.nil?} #{new_resource.name} && rbenv rehash"
     environment  ({'HOME' => user_root})
     not_if "ls #{user_root}/.rbenv/versions | grep #{new_resource.name}"
   end
